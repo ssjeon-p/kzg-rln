161f141e40687cb56511a8a1b6ac95730acc64b1
SJPC <ssjeon@postech.ac.kr>
2023-09-20 15:23:42 +0900

    kzg

--- versionB/src/kzg.rs
+++ versionB/src/kzg.rs
@@ -1,14 +1,14 @@
+use std::ops::SubAssign;
+
 use halo2::{
-    poly::{kzg::commitment::{KZGCommitmentScheme, ParamsKZG, ParamsVerifierKZG},
+    poly::{kzg::commitment::ParamsKZG,
     commitment::{ParamsProver, Blind}, EvaluationDomain},
-    halo2curves::{bn256::{Fr, Bn256, G1Affine, G2Affine}, pairing::{self, Engine}, group::cofactor::CofactorCurveAffine},
-    arithmetic::Field,
+    halo2curves::{bn256::{Fr, Bn256, G1Affine, G2Affine}, pairing::Engine, group::Curve},
+    arithmetic::{Field, eval_polynomial},
 };
 
 struct KZG {
-    keys: ParamsKZG<Bn256>,
-    domain: EvaluationDomain<Fr>,
-    
+    keys: ParamsKZG<Bn256>,    
 }
 
 pub fn commit(
@@ -26,19 +26,32 @@ pub fn witness_polynomial(
     poly: Vec<Fr>,
     b: Fr,
 ) -> G1Affine {
-    b = -b;
+    let eval = eval_polynomial(&poly, b); 
+    let mut ppo = poly.clone();
     let a = poly.into_iter();
 
-    let mut q = vec![Fr::ZERO; a.len() - 1];
+    let mut q = vec![Fr::ZERO; a.len()];
 
     let mut tmp = Fr::ZERO;
     for (q, r) in q.iter_mut().rev().zip(a.rev()) {
-        let mut lead_coeff = *r;
-        lead_coeff.sub_assign(&tmp);
+        let mut lead_coeff = r;
+        lead_coeff -= tmp;
         *q = lead_coeff;
         tmp = lead_coeff;
-        tmp -= b;
+        tmp *= -b;
     }
+    q[3] = Fr::ZERO;
+
+    let mul = vec![
+        -b * q[0],
+        -b * q[1] + q[0],
+        -b * q[2] + q[1],
+        q[2],
+    ];
+    ppo[0] += eval;
+    assert_eq!(mul, ppo);
+    
+
 
     commit(&keys, q)
 }
@@ -48,13 +61,41 @@ pub fn verify_proof(
     proof: G1Affine,
     commit: G1Affine,
     b: Fr,
+    eval: Fr,
 ) -> bool {
+    let g = G1Affine::generator();
+    let h = G2Affine::generator();
+    let lhs = Bn256::pairing(&commit, &h);
+    let h_to_alpha_minus_b = (keys.s_g2() - h * b).to_affine();
+    let rhs = Bn256::pairing(&proof, &h_to_alpha_minus_b) + Bn256::pairing(&g, &h) * eval;
 
+    lhs == rhs
 }
 
-pub fn pairing_check (
-    keys: &ParamsKZG<Bn256>,
-    pairing,
-) {
-    Bn256::pairing(&G1Affine::identity(), &G2Affine::identity());
+#[cfg(test)]
+mod test {
+    use halo2::arithmetic::eval_polynomial;
+    use rand::thread_rng;
+    use super::*;
+
+    #[test]
+    fn test_kzg() {
+        let rng = &mut thread_rng();
+        let keys = ParamsKZG::<Bn256>::new(2);
+        let domain = EvaluationDomain::<Fr>::new(1, 2);
+        let coeffs = vec![
+            Fr::random(rng.clone()),
+            Fr::random(rng.clone()),
+            Fr::random(rng.clone()),
+            Fr::random(rng.clone()),
+        ];
+        let commit = commit(&keys, coeffs.clone());
+        
+        let b = Fr::ZERO;
+        let proof = witness_polynomial(&keys, coeffs.clone(), b);
+
+        let eval = eval_polynomial(&coeffs, b);
+        
+        //assert!(verify_proof(&keys, proof, commit, b, eval));
+    }
 }
